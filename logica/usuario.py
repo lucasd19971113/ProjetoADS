3ec219f3631734a635bcc20bf06d95633860933a
lucasd19971113 <lucas_d_motta@outlook.com>
2017-11-01 19:07:38 -0200

    Teste2

--- logica/usuario.py
+++ logica/usuario.py
@@ -34,3 +34,4 @@ def remover_usuario(cpf):
 def iniciar_clientes():
     adicionar_usuario(43356795805,"André","andre.oliveria@gmail.com","12341234")
     adicionar_usuario(43356725805, "Lucas", "lucas.dias@hotmail.com", "34123412")
+                                
