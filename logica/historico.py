0f5fba1af3850f34953509142e53c80da75be0d5
andreoliveira <castrodeko11@hotmail.com>
2017-11-04 16:04:53 -0200

    "Módulo histórico"

--- logica/historico.py
+++ logica/historico.py
@@ -8,6 +8,6 @@ def listar_historicos():
     return historicos
 
 
-def iniciar_hisotico():
+def iniciar_historico():
     adicionar_historico(43356795805,5)
     adicionar_historico(43356725805, 3)
