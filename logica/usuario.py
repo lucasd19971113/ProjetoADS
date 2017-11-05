c1bd6cad9054ec126f1fe052db1c48198be415e0
lucasd19971113 <lucas_d_motta@outlook.com>
2017-11-05 00:43:31 -0200

    login admin e login usuario adicionados ao módulo usuário

--- logica/usuario.py
+++ logica/usuario.py
@@ -32,6 +32,38 @@ def remover_usuario(cpf):
         del clientes[pos]
         return True
 
+def login_cliente(email,senha):
+    email_1 = [(index, row.index(email)) for index, row in enumerate(clientes) if email in row]
+    if email_1 == []:
+        return False
+    linha=email_1[0][0]
+    col = email_1[0][1]
+    if linha==0 or linha == 1 and col == 2:
+        return False
+    else:
+        cliente=clientes[linha][3]
+        if senha != cliente:
+            return False
+        else:
+            return True
+
+
+def login_admin(email,senha):
+    email_1 = [(index, row.index(email)) for index, row in enumerate(clientes) if email in row]
+    if email_1 == []:
+        return False
+    linha=email_1[0][0]
+    col = email_1[0][1]
+    if linha > 1 and col == 2:
+        return False
+    else:
+        admin=clientes[linha][3]
+        if senha != admin:
+            return False
+        else:
+            return True
+
+
 
 def iniciar_clientes():
     adicionar_usuario(43356795805,"André","andre.oliveria@gmail.com","12341234")
